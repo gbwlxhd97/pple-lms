953c44632ee76cab9cefffb22fd09b664fc189e5
eunhye <grace32612@naver.com>
2024-05-29 23:16:27 +0900

    NewIcon add

--- src/components/common/Table2/Table2.tsx
+++ src/components/common/Table2/Table2.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import styles from './Table2.module.scss';
+import NewIcon from '@/icons/icon/NewIcon';
 
 interface Table2Props {
   tableHead: string[]; // 테이블의 각 열 제목
   tableBody: any[]; // 각 행의 데이터 배열
+  isShowNew?: boolean;
 }
 
-const Table2 = ({ tableBody, tableHead }: Table2Props) => {
+const Table2 = ({ tableBody, tableHead, isShowNew }: Table2Props) => {
   return (
     <table style={{ width: '100%' }}>
       <thead className={styles.TableHead}>
@@ -23,9 +25,14 @@ const Table2 = ({ tableBody, tableHead }: Table2Props) => {
           <tr key={rowIndex}>
             <td className={styles.TableNum}>{rowIndex + 1}</td>
             <td className={styles.TableTitle}>
-              {row.value1}이것은 공지사항입니다.
+              {row.title}이것은 공지사항입니다.
+              {isShowNew && (
+                <div className={styles.NewIcon}>
+                  <NewIcon width={12} height={12} />
+                </div>
+              )}
             </td>
-            <td className={styles.TableDate}>{row.value2}2024.03.02</td>
+            <td className={styles.TableDate}>{row.createdAt}2024.03.02</td>
           </tr>
         ))}
       </tbody>
