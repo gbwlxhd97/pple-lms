1410ec5574c683d53b7f8392c8a5fc6c76a33b0b
gbwlxhd97 <gbwlxhd97@naver.com>
2024-06-04 14:18:50 +0900

    feat : drag file wip..

--- src/pages/Course/Reference/Edit/index.tsx
+++ src/pages/Course/Reference/Edit/index.tsx
@@ -1,15 +1,44 @@
+import Input from '@/components/common/Input/Input';
+import TextArea from '@/components/common/TextArea/TextArea';
 import Title from '@/components/common/Title/Title'
 import useProfileStore from '@/stores/useProfileStore';
-
+import { FileUploader } from 'react-drag-drop-files';
+import styles from './index.module.scss'
+import { useState } from 'react';
 const CourseReferenceEditPage = () => {
   const {
     profile: { role },
   } = useProfileStore();
+  const [file, setFile] = useState<any>(null);
+
+  const handleChange = (file:any) => {
+    setFile(file);
+  };
+  const fileTypes = ['JPEG', 'PNG', 'GIF'];
   return (
     <div className="SpacingWrapper">
-      <Title title='강의 자료 업로드'/>
+      <Title title="강의 자료 업로드" />
+      <Input type="text" />
+      <TextArea />
+      <div className={styles.FileUploaderContainer}>
+        <FileUploader
+          handleChange={handleChange}
+          name="file"
+          types={fileTypes}
+          className={styles.CustomUploader}
+        >
+          <div className={styles.CustomMessage}>
+            <p>파일을 여기로 드래그하거나 클릭하여 업로드하세요.</p>
+          </div>
+        </FileUploader>
+        {file && (
+          <div className="preview">
+            <p>업로드된 파일: {file.name}</p>
+          </div>
+        )}
+      </div>
     </div>
-  )
+  );
 }
 
 export default CourseReferenceEditPage
