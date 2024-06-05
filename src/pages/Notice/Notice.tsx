29bf43e4e72a7c4842491eb79fb6dadadacefc57
gbwlxhd97 <gbwlxhd97@naver.com>
2024-06-05 22:58:24 +0900

    setting api notice

--- src/pages/Notice/Notice.tsx
+++ src/pages/Notice/Notice.tsx
@@ -2,31 +2,31 @@ import styles from './Notice.module.scss';
 import Title from '@/components/common/Title/Title';
 import Table2 from '@/components/common/Table2/Table2';
 import useProfileStore from '@/stores/useProfileStore';
+import { useEffect, useState } from 'react';
+import noticeAPIList from '@/services/notice';
+import { INoticeList } from '@/interfaces/notice';
 
 const NoticePage = () => {
   const {
     profile: { role },
   } = useProfileStore();
+  const [noticeList,setNoticeList] = useState<Array<INoticeList>>()
+  
+  const getNoticeList = async () => {
+    const res = await noticeAPIList.getNoticeList()
+    setNoticeList(res);
+  }
 
-  const noticeList = [
-    { id: 1, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 2, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 3, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 4, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 5, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 6, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 7, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 8, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 9, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-    { id: 10, title: '이것은 공지사항입니다.', createdAt: '2024.04.18' },
-  ];
+  useEffect(() => {
+    getNoticeList();
+  },[])
   return (
     <>
       <Title title="공지사항" isShowButton={role === 'TEACHER'} path='/notice/edit' />
       <div className={styles.NoticeTable}>
         <Table2
           tableHead={['번호', '제목', '날짜']}
-          tableBody={noticeList}
+          tableBody={noticeList || []}
           isShowNew={true}
           path="/notice/detail"
         />
--- src/services/notice.ts
+++ src/services/notice.ts
@@ -3,15 +3,17 @@ import { requestAPI } from '@/utils/fetch';
 type insertRequestBody = {
   title: string;
   main: string;
-  courseId : number;
-}
+  toStudent: boolean;
+  toParent: boolean;
+};
+
 const insertNotice = async (requestBody: insertRequestBody) => {
   const data = await requestAPI().post('/notice/write',requestBody)
   return data;
 }
 
 const getNoticeList = async () => {
-  const data = await requestAPI().get('/notice/readList')
+  const {data} = await requestAPI().get('/notice/readList')
   return data;
 }
 
