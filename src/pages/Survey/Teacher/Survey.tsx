1118b998a04fcb5cea50116dfdb2177c193cf065
eunhye <grace32612@naver.com>
2024-06-18 17:34:08 +0900

    Teacher Survey List Add

---
+++ src/pages/Survey/Teacher/Survey.tsx
@@ -0,0 +1,56 @@
+import styles from './Survey.module.scss';
+import Title from '@/components/common/Title/Title';
+import Table3 from '@/components/common/Table3/Table3';
+import useProfileStore from '@/stores/useProfileStore';
+import { useEffect, useState } from 'react';
+import surveyAPIList from '@/services/survey';
+import { ISurveyTeacherList } from '@/interfaces/survey';
+import { useParams } from 'react-router';
+import { today } from '@/utils/date';
+
+const SurveyTeacherPage = () => {
+  const {
+    profile: { role },
+  } = useProfileStore();
+
+  const { courseId, surveyId } = useParams();
+
+  const [surveyList, setSurveyList] = useState<Array<ISurveyTeacherList>>();
+
+  const getSurveyList = async (courseId: number) => {
+    try {
+      const res = await surveyAPIList.getSurveyList(courseId);
+      const updatedData = res.map((item: ISurveyTeacherList) => ({
+        ...item,
+        isNew: item.createdAt === today,
+      }));
+      console.log(updatedData);
+      setSurveyList(updatedData);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    if (courseId) {
+      getSurveyList(parseInt(courseId));
+    }
+  }, [courseId]);
+
+  return (
+    <>
+      <Title
+        title="설문조사"
+        isShowButton={role === 'TEACHER'}
+        path="/survey/edit"
+      />
+      <div className={styles.SurveyTable}>
+        <Table3
+          tableHead={['제목', '날짜']}
+          tableBody={surveyList || []}
+          path={`/course/${courseId}/survey/${surveyId}/take-survey`}
+        />
+      </div>
+    </>
+  );
+};
+
+export default SurveyTeacherPage;
